fb15c858ce8727bbfd57c8f2730c80c319b8a8d3
thisconnect <p7tryk@gmail.com>
2020-01-16 22:49:54 +0100

    gracze first

--- gracz.h
+++ gracz.h
@@ -6,13 +6,44 @@ class gracz
   int zycia;
 
  public:
+  gracz();
   void idz_gora();
+  void idz_gora(int kroki);
   void idz_dol();
 
   void update();
-
+  void status();
   
 };
-
+gracz::gracz()
+{
+  position=0;
+  zycia=7;
+  update();
+}
+void gracz::update()
+{
+  if(position>80)
+    position=80;
+  punkty=position*10;
+}
+void gracz::idz_gora()
+{
+  int kroki=0;
+  printf("podaj liczba krokow w gore\n");
+  scanf("%d",&kroki);
+  position+=kroki;
+  update();
+}
+void gracz::idz_gora(int kroki)
+{
+  position+=kroki;
+  update();
+}
+void gracz::status()
+{
+  update();
+  printf("pozycja %d, punkty %d, zycia %d\n",position,punkty,zycia);
+}
 
 
--- gracze.cpp
+++ gracze.cpp
@@ -1,20 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "gracz.h"
+bool istnieje(const int liczbagraczy,const int n)
+{
+  if(n>liczbagraczy)
+    {
+      printf("nie ma takiego gracza\n");
+      return false;
+    }
+  else
+    return true;
+}
+
 
 int main()
 {
   bool status = true;
-  
+  int liczbagraczy=0;
+  gracz *listagraczy;
   while(true)
     {
-      int mode;
-      printf("wybierz opcje\n1 utworz graczy\n2usun graczy\n3przesun w gore\n4przesun w dol\n5 pokaz pozycje\n9 koniec");
+      int mode=0;
+      int n=0;
+      printf("wybierz opcje\n1 utworz graczy\n2 usun graczy\n3 przesun w gore\n4 przesun w dol\n5 pokaz pozycje\n6 przysun wszystkich w gore\n9 wypisz wszyskich\n10 koniec");
       scanf("%d",&mode);
       switch(mode)
 	{
 	case 1:
-	  
+	  printf("podaj liczbe graczy\n");
+	  scanf("%d",&liczbagraczy);
+	  listagraczy = new gracz[liczbagraczy];
+	  break;
+	case 2:
+	  delete[] listagraczy;
+	  liczbagraczy=0;
+	  break;
+	case 3:
+	  printf("wybierz gracza do przesuniecia od 1 do %d\n",liczbagraczy);
+	  scanf("%d",&n);
+	  if(istnieje(liczbagraczy,n))
+	    {
+	      listagraczy[n].idz_gora();
+	    }
+	  break;
+	case 5:
+	  printf("wybierz gracza do wyswietlenia od 1 do %d\n",liczbagraczy);
+	  scanf("%d",&n);
+	  if(istnieje(liczbagraczy,n))
+	    {
+	      listagraczy[n].status();
+	    }
+	  break;
+	case 6:
+	  printf("o ile przesunac wszystkich\n");
+	  scanf("%d",&n);
+	  for(int i=0;i<liczbagraczy;i++)
+	    {
+	      listagraczy[i].idz_gora(n);
+	    }
+	  break;
+	case 9:
+	  for(int i=0;i<liczbagraczy;i++)
+	    {
+	      printf("gracz %d ",i);
+	      listagraczy[i].status();
+	    }
+	  break;
+	case 10:
+	  delete[] listagraczy;
+	  return false;
+	default:
+	  break;
 	}
       
     }
